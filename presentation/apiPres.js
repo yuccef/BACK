b22cdc7d2371327dd2e010a408ecca939dab89e7
youssef ABBADI <youssefabbadipro@gmail.com>
2023-03-14 15:14:05 +0100

    Updating apiPres.js

--- presentation/apiPres.js
+++ presentation/apiPres.js
@@ -8,6 +8,12 @@ const apiPres = {
 
         app.use(express.json());
 
+        app.use(function(req, res, next) {  
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+        });
+
         app.get("/test", function(req, res){
             const testObj ={
                 test: "test"
