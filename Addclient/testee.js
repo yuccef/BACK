33523efddad8025b6995fc829be7a01a32abf883
youssef ABBADI <youssefabbadipro@gmail.com>
2023-04-02 18:36:29 +0200

    Updates

--- Addclient/testee.js
+++ Addclient/testee.js
@@ -2,25 +2,76 @@ const fs = require('fs');
 
 // Lecture du fichier JSON
 const rawData = fs.readFileSync('donnees.json');
-let utilisateurs = JSON.parse(rawData);
+let data = JSON.parse(rawData);
 
 
-function enregistrerNouvelUtilisateur() {
-  const formulaire = document.getElementById('idd'); // Remplacez 'mon-formulaire' par l'ID de votre formulaire
 
-  const nouvelUtilisateur = {
-    id: Math.floor(Math.random() * 10000000), // Génère un ID aléatoire
-    email: formulaire.email.value,
-    first: formulaire.prenom.value,
-    last: formulaire.nom.value,
-    company: formulaire.societe.value,
-    created_at: new Date().toISOString(),
-    country: formulaire.pays.value
-  };
 
-  console.log(nouvelUtilisateur); // Affiche le nouvel utilisateur dans la console
-  // Ajoutez ici le code pour envoyer l'utilisateur au serveur ou faire autre chose avec les données
+// Ajout du nouveau client
+const nouveauClient = {
+  "id": 222222,
+  "email": "yregtrfg",
+  "first": "yuccef",
+  "last": "rfedd",
+  "company": "orio",
+  "created_at": "2002-11-10T00:00:00.000Z",
+  "country": "morocco"
+};
 
-  formulaire.reset(); // Réinitialise le formulaire
-}
-utilisateurs.push(nouvelUtilisateur);
+tab.push(nouveauClient);
+var newdata = JSON.stringify(tab);
+fs.writeFile("users.json", newdata, err => {
+    // error checking
+    if(err) throw err;
+    
+    console.log("Utilisateur ajouté");
+});
+
+
+
+
+// app.use(bodyParser.json());
+
+// // Route HTTP POST pour enregistrer les nouvelles données utilisateur
+// app.post('/utilisateurs', (req, res) => {
+//   const nouvelUtilisateur = {
+//     id: Math.floor(Math.random() * 10000000),
+//     email: req.body.email,
+//     first: req.body.prenom,
+//     last: req.body.nom,
+//     company: req.body.societe,
+//     created_at: new Date().toISOString(),
+//     country: req.body.pays
+//   };
+
+//   // Lecture du fichier JSON existant
+//   fs.readFile('donnees.json', 'utf8', (err, data) => {
+//     if (err) {
+//       console.error(err);
+//       res.status(500).send('Erreur du serveur');
+//       return;
+//     }
+
+//     let utilisateurs = JSON.parse(data);
+
+//     // Ajout du nouvel utilisateur
+//     utilisateurs.push(nouvelUtilisateur);
+
+//     // Écriture des données mises à jour dans le fichier JSON
+//     fs.writeFile('donnees.json', JSON.stringify(utilisateurs), (err) => {
+//       if (err) {
+//         console.error(err);
+//         res.status(500).send('Erreur du serveur');
+//         return;
+//       }
+
+//       // Réponse de réussite à la requête
+//       res.status(201).send('Nouvel utilisateur enregistré');
+//     });
+//   });
+// });
+
+// // Lancement du serveur Node.js
+// app.listen(port, () => {
+//   console.log(`Serveur lancé sur le port ${port}`);
+// });
