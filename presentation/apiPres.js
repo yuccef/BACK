ea0f67520336b36cfe888894d1b69b3344ae13a3
youssef ABBADI <youssefabbadipro@gmail.com>
2023-04-21 15:08:20 +0200

    Synchronize server and database

--- presentation/apiPres.js
+++ presentation/apiPres.js
@@ -7,6 +7,8 @@ const fs = require('fs');
 const data = fs.readFileSync('./data/customers.json');
 const customers = JSON.parse(data);
 const bodyParser = require('body-parser');
+const chokidar = require('chokidar');
+const axios = require('axios');
 
 app.use(express.static(path.join(__dirname, '/../public')));
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -21,61 +23,95 @@ const apiServ = {
             origin: '*'
         }));
 
-        /** Middleware pour le rafraîchissement de la page HTML */
-        app.use(function(req, res, next) {
-            res.setHeader('Cache-Control', 'no-cache');
-            next();
-        });
 
-        app.get('/api/customers', (req, res) => {
-            fs.readFile('./data/customers.json', (err, data) => {
-                res.json(customers);
+
+app.get('/api/customers', (req, res) => {
+    fs.readFile('./data/customers.json', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.sendStatus(500);
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+const customersWatcher = chokidar.watch('./data/customers.json');
+customersWatcher.on('change', (path) => {
+    console.log(`File ${path} has been changed`);
+    // Lire le fichier JSON modifié
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        // Actualiser la base de données du serveur API
+        const customers = JSON.parse(data);
+        // Code pour actualiser la base de données du serveur API ici
+    });
+});
+
+const axios = require('axios');
+  
+app.get('/api/customers/id/:id', (req, res) => {
+    const id = req.params.id;
+    const customer = customers.find(c => c.id === Number(id));
+    if (customer) {
+        res.json(customer);
+    } else {
+        res.status(404).json({ message: 'Customer not found' });
+    }
+});
+
+app.put('/api/customers/id/:id', (req, res) => {
+    const id = req.params.id;
+    const customer = customers.find(c => c.id === Number(id));
+    if (customer) {
+        customer.first = req.body.first;
+        customer.last = req.body.last;
+        customer.email = req.body.email;
+        customer.company = req.body.company;
+        customer.country = req.body.country;
+
+        // Envoyer une requête pour mettre à jour la base de données locale
+        axios.put(`http://localhost:3001/api/customers/${id}`, req.body)
+            .then(() => {
+                res.json(req.body);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.sendStatus(500);
             });
-        });
+    } else {
+        res.status(404).json({ message: 'Customer not found' });
+    }
+});
 
-        app.get('/api/customers/id/:id', (req, res) => {
-            const id = req.params.id;
-            const customer = customers.find(c => c.id === Number(id));
-            if (customer) {
-                res.json(customer);
-            } else {
-                res.status(404).json({ message: 'Customer not found' });
+// Route pour mettre à jour la base de données locale
+app.put('/api/customers/:id', (req, res) => {
+    const id = req.params.id;
+    const customerIndex = customers.findIndex(c => c.id === Number(id));
+    if (customerIndex !== -1) {
+        customers[customerIndex] = {
+            id: Number(id),
+            first: req.body.first,
+            last: req.body.last,
+            email: req.body.email,
+            company: req.body.company,
+            country: req.body.country,
+        };
+        // Écrire les modifications dans le fichier JSON local
+        fs.writeFile('./data/customers.json', JSON.stringify(customers), (err) => {
+            if (err) {
+                console.error(err);
+                return res.sendStatus(500);
             }
+            res.sendStatus(200);
         });
+    } else {
+        res.status(404).json({ message: 'Customer not found' });
+    }
+});
 
-        app.put('/api/customers/id/:id', (req, res) => {
-            const id = req.params.id;
-            const customer = customers.find(c => c.id === Number(id));
-            if (customer) {
-                customer.first = req.body.first;
-                customer.last = req.body.last;
-                customer.email = req.body.email;
-                customer.company = req.body.company;
-                customer.country = req.body.country;
-
-                business.UpDater(req.body);
-                res.json(req.body);  
-
-            } else {
-                res.status(404).json({ message: 'Customer not found' });
-            }
-        });
-
-        app.get('/api/customers/liste', function(req, res) {
-            res.sendFile(path.join(__dirname, '/../public/list.html'));
-        });
-
-        app.get('/api/customers/liste/add', function(req, res) {
-            res.sendFile(path.join(__dirname, '/../public/add.html'));
-        });
-
-        app.get('/api/customers/liste/modify', function(req, res) {
-            res.sendFile(path.join(__dirname, '/../public/modify.html'));
-        });
-
-        app.get('/api/customers/liste/delete', function(req, res) {
-            res.sendFile(path.join(__dirname, '/../public/delete.html'));
-        });
 
         app.delete('/api/customers', (req, res) => {
             business.deleteUser(req.query.id);
@@ -93,6 +129,28 @@ const apiServ = {
         });
 
 
+        app.get('/api/customers/liste', function(req, res) {
+            res.sendFile(path.join(__dirname, '/../public/list.html'));
+        });
+
+        app.get('/api/customers/liste/add', function(req, res) {
+            res.sendFile(path.join(__dirname, '/../public/add.html'));
+        });
+
+        app.get('/api/customers/liste/modify', function(req, res) {
+            res.sendFile(path.join(__dirname, '/../public/modify.html'));
+        });
+
+        app.get('/api/customers/liste/delete', function(req, res) {
+            res.sendFile(path.join(__dirname, '/../public/delete.html'));
+        });
+
+
+        // app.get('/api/customers', (req, res) => {
+        //     fs.readFile('./data/customers.json', (err, data) => {
+        //         res.json(customers);
+        //     });
+        // });
 
             
         /**Server running on port */
